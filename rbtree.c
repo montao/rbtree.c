df7fc780594a7d0944725b947b1b395ef620f409
niklasr <niklasr@protonmail.com>
2018-11-17 10:23:16 +0100

    Increase test coverage

--- rbtree.c
+++ rbtree.c
@@ -357,7 +357,8 @@ void traversal(struct node *root) {
 }
 
 int main() {
-    int T = 10000; //test case 10000 nodes
+    clock_t start = clock(), diff;
+    int T = 10000000; //test case 10000 nodes
     int r2;
     struct node *root = NULL;
     srand(time(NULL));
@@ -378,7 +379,24 @@ int main() {
             z->color = RED;
             root = insert(root, z);
         } else printf("malloc failed at node number %d", T);
+
+        z->key = 999999;
+        z->left = NULL;
+        z->right = NULL;
+        z->parent = NULL;
+        z->color = RED;
+        root = insert(root, z);
+
     }
+    diff = clock() - start;
+
+    int msec = diff * 1000 / CLOCKS_PER_SEC;
+    printf("Time taken %d seconds %d milliseconds", msec/1000, msec%1000);
+    clock_t start2 = clock(), diff2;
+    search(root, 999999);
+    diff2 = clock() - start2;
+    int msec2 = diff2 * 1000 / CLOCKS_PER_SEC;
+    printf("Lookup taken %d seconds %d milliseconds", msec2/1000, msec2%1000);
     root = NULL;
     return 0;
 }
